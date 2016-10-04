276dd7238aaa1f45cc6eda5d67c575333f050bee
wonderwrz0606 <wonderwrz0606@gmail.com>
2016-10-04 14:36:55 -0400

    init

--- js/index.js
+++ js/index.js
@@ -1,14 +1,6 @@
 $(function() {
-		var selectedClass = "";
-		$(".fil-cat").click(function(){
-		selectedClass = $(this).attr("data-rel");
-     $("#portfolio").fadeTo(100, 0.1);
-		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
-    setTimeout(function() {
-      $("."+selectedClass).fadeIn().addClass('scale-anm');
-      $("#portfolio").fadeTo(300, 1);
-    }, 300);
-	});
+	$('#photos').hide();
+	$('#photos').delay(15000).fadeIn('slow');
 });
 
 $(function(){
@@ -30,5 +22,5 @@ $(function(){
 	      $(this).detach();
 	    }
 	  });
-	},600);
+	},3000);
 });
